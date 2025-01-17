189c4485a81b4ee05801c43b7907ec451e28c8d9
Pablo Benavides <pbenav@gmail.com>
2025-01-17 18:22:49 +0100

    Added CopyRight (left) ;-)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,28 +3,28 @@
         class="flex justify-center py-4 min-h-screen bg-gray-100 login flexrelative items-top dark:bg-gray-900 sm:items-center sm:pt-0">
         <!-- Topbar login and register links -->
         @if (Route::has('login'))
-            <div class="flex fixed top-0 right-0 px-6 py-4">
-                @auth
-                    <a href="{{ url('events') }}"
-                        class="text-sm text-gray-700 underline dark:text-gray-500">{{ __('Events') }}</a>
-                    <form class="inline-flex" method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <a href="#" class="ml-4 text-sm text-gray-700 underline dark:text-gray-500"
-                            onclick="event.preventDefault();
+        <div class="flex fixed top-0 right-0 px-6 py-4">
+            @auth
+            <a href="{{ url('events') }}"
+                class="text-sm text-gray-700 underline dark:text-gray-500">{{ __('Events') }}</a>
+            <form class="inline-flex" method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a href="#" class="ml-4 text-sm text-gray-700 underline dark:text-gray-500"
+                    onclick="event.preventDefault();
                                             this.closest('form').submit()">
-                            {{ __('Logout') }}
-                        </a>
-                    </form>
-                @else
-                    <a href="{{ route('login') }}"
-                        class="text-sm text-gray-700 underline dark:text-gray-500">{{ __('Log in') }}</a>
+                    {{ __('Logout') }}
+                </a>
+            </form>
+            @else
+            <a href="{{ route('login') }}"
+                class="text-sm text-gray-700 underline dark:text-gray-500">{{ __('Log in') }}</a>
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 underline dark:text-gray-500">{{ __('Register') }}</a>
-                    @endif
-                @endauth
-            </div>
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}"
+                class="ml-4 text-sm text-gray-700 underline dark:text-gray-500">{{ __('Register') }}</a>
+            @endif
+            @endauth
+        </div>
         @endif
 
         <!-- Main content -->
@@ -40,32 +40,32 @@
                         <div class="flex items-center">
 
                             @if (auth()->check())
-                                <a href="{{ route('events') }}" class="ml-1">
-                                    <i class="ml-1 fas fa-sign-in"></i>
-                                    {{ __('Events') }}
-                                </a>
+                            <a href="{{ route('events') }}" class="ml-1">
+                                <i class="ml-1 fas fa-sign-in"></i>
+                                {{ __('Events') }}
+                            </a>
 
-                                <a href="{{ route('logout') }}" class="ml-3">
-                                    <i class="ml-1 fas fa-user-plus"></i>
-                                    {{ __('Logout') }}
-                                </a>
+                            <a href="{{ route('logout') }}" class="ml-3">
+                                <i class="ml-1 fas fa-user-plus"></i>
+                                {{ __('Logout') }}
+                            </a>
                             @else
-                                <a href="{{ route('login') }}" class="ml-1">
-                                    <i class="ml-1 fas fa-sign-in"></i>
-                                    {{ __('Login') }}
-                                </a>
+                            <a href="{{ route('login') }}" class="ml-1">
+                                <i class="ml-1 fas fa-sign-in"></i>
+                                {{ __('Login') }}
+                            </a>
 
-                                <a href="{{ route('register') }}" class="ml-3">
-                                    <i class="ml-1 fas fa-user-plus"></i>
-                                    {{ __('Register') }}
-                                </a>
+                            <a href="{{ route('register') }}" class="ml-3">
+                                <i class="ml-1 fas fa-user-plus"></i>
+                                {{ __('Register') }}
+                            </a>
                             @endif
                         </div>
                     </div>
 
                     <!-- Versions -->
                     <div class="ml-4 text-sm text-center text-gray-500">
-                        {{ config('app.name') }} v{{ env('APP_VER') }} {{-- (PHP v{{ PHP_VERSION }}) --}}
+                        <a href="https://cv.sientia.com" target="_blank">©{{ config('app.name') }} v{{ env('APP_VER') }} {{-- (PHP v{{ PHP_VERSION }}) --}} </a>
                     </div>
                 </div>
             </div>
