d8b9bb1c7cad3d04aef0dd29fff99c71bf31566a
Pablo Benavides <pbenav@gmail.com>
2025-09-11 18:50:32 +0200

    Fix is_authorized checkbox behavior

--- resources/views/livewire/events/get-time-registers.blade.php
+++ resources/views/livewire/events/get-time-registers.blade.php
@@ -21,7 +21,8 @@
     @if (session('info'))
         <div class="flex items-center px-4 py-3 text-sm font-bold text-white bg-blue-500" role="alert">
             <svg class="mr-2 w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+                <path
+                    d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
             </svg>
             <p>{{ __(session('info')) }}</p>
         </div>
@@ -38,7 +39,8 @@
             @livewire('add-event')
             @if (!$isInspector || $isTeamAdmin)
                 <div class="pl-0 mx-auto w-48 sm:mx-0">
-                    <x-jet-button class="justify-center w-full h-16 bg-green-500 hover:bg-green-600 disabled:bg-gray-500"
+                    <x-jet-button
+                        class="justify-center w-full h-16 bg-green-500 hover:bg-green-600 disabled:bg-gray-500"
                         wire:click="$emitTo('add-event', 'add', '1')">
                         <i class="mr-2 fas fa-plus"></i> {{ __('Add event') }}
                     </x-jet-button>
@@ -54,7 +56,8 @@
                 </x-jet-button>
             </div>
             <div class="mx-auto w-48 sm:mx-0">
-                <x-jet-button class="w-48 h-8 justify-center whitespace-nowrap {{ $filtered ? 'bg-green-500' : 'disabled' }}"
+                <x-jet-button
+                    class="w-48 h-8 justify-center whitespace-nowrap {{ $filtered ? 'bg-green-500' : 'disabled' }}"
                     wire:click="unsetFilter">
                     <i class="fa-solid fa-filter-slash"></i>
                     <i class="mr-2 fas fa-x"></i>{{ __('Unset filter') }}
@@ -64,7 +67,8 @@
             <!-- Search Input and Checkbox -->
             <div class="flex flex-nowrap">
                 <div class="w-auto">
-                    <x-jet-input class="w-full h-8" placeholder="{{ __('Search') }}" type="text" wire:model="search" />
+                    <x-jet-input class="w-full h-8" placeholder="{{ __('Search') }}" type="text"
+                        wire:model="search" />
                 </div>
                 <div class="flex flex-row-reverse pt-1 ml-4 w-auto">
                     <div>
@@ -99,7 +103,8 @@
                             <th class="p-1 text-center text-white bg-gray-600 md:table-cell">{{ __('Id') }}</th>
 
                             @if ($isTeamAdmin || $isInspector)
-                                <th class="p-1 text-center text-white bg-gray-600 md:table-cell" wire:click="order('name')">
+                                <th class="p-1 text-center text-white bg-gray-600 md:table-cell"
+                                    wire:click="order('name')">
                                     {{ __('Worker') }}
                                     <i class="float-right mt-1 fas fa-sort"></i>
                                 </th>
@@ -115,16 +120,19 @@
                                 <i class="float-right mt-1 fas fa-sort"></i>
                             </th>
 
-                            <th class="p-1 text-left text-white bg-gray-600 md:table-cell" wire:click="order('description')">
+                            <th class="p-1 text-left text-white bg-gray-600 md:table-cell"
+                                wire:click="order('description')">
                                 {{ __('Description') }}
                                 <i class="float-right mt-1 fas fa-sort"></i>
                             </th>
 
                             <th class="p-1 text-center text-white bg-gray-600 md:table-cell">{{ __('Duration') }}</th>
-                            <th class="p-1 text-center text-white bg-gray-600 md:table-cell">{{ __('Authorized') }}</th>
+                            <th class="p-1 text-center text-white bg-gray-600 md:table-cell">{{ __('Authorized') }}
+                            </th>
 
                             @if (!$isInspector || $isTeamAdmin)
-                                <th class="p-1 text-center text-white bg-gray-600 md:table-cell">{{ __('Actions') }}</th>
+                                <th class="p-1 text-center text-white bg-gray-600 md:table-cell">{{ __('Actions') }}
+                                </th>
                             @endif
                         </tr>
                     </thead>
@@ -132,19 +140,24 @@
                     <tbody class="block md:table-row-group">
                         @foreach ($events as $ev)
                             <tr class="block border md:table-row">
-                                <td class="p-1 text-center md:table-cell" style="background-color: {{ $ev->eventType->color ?? 'transparent' }}; color: {{ $ev->eventType && $this->isDark($ev->eventType->color) ? 'white' : 'black' }}">
+                                <td class="p-1 text-center md:table-cell"
+                                    style="background-color: {{ $ev->eventType->color ?? 'transparent' }}; color: {{ $ev->eventType && $this->isDark($ev->eventType->color) ? 'white' : 'black' }}">
                                     {{ $ev->id }}
                                 </td>
 
                                 @if ($isTeamAdmin || $isInspector)
-                                    <td class="p-1 text-left md:table-cell">{{ $ev->user->name . ' ' . $ev->user->family_name1 }}</td>
+                                    <td class="p-1 text-left md:table-cell">
+                                        {{ $ev->user->name . ' ' . $ev->user->family_name1 }}</td>
                                 @endif
 
-                                <td class="p-1 text-left md:table-cell">{{ Carbon\Carbon::parse($ev->start)->format('d/m/y H:i:s') }}</td>
-                                <td class="p-1 text-left md:table-cell">{{ $ev->end ? Carbon\Carbon::parse($ev->end)->format('d/m/y H:i:s') : '' }}</td>
+                                <td class="p-1 text-left md:table-cell">
+                                    {{ Carbon\Carbon::parse($ev->start)->format('d/m/y H:i:s') }}</td>
+                                <td class="p-1 text-left md:table-cell">
+                                    {{ $ev->end ? Carbon\Carbon::parse($ev->end)->format('d/m/y H:i:s') : '' }}</td>
                                 <td class="p-1 text-left md:table-cell">
                                     @if ($ev->eventType)
-                                        <span class="inline-block w-3 h-3 mr-2 rounded-full" style="background-color: {{ $ev->eventType->color }}"></span>
+                                        <span class="inline-block w-3 h-3 mr-2 rounded-full"
+                                            style="background-color: {{ $ev->eventType->color }}"></span>
                                         <span>{{ $ev->eventType->name }}</span>
                                     @else
                                         {{ __($ev->description) }}
@@ -153,24 +166,24 @@
                                 <td class="p-1 text-center md:table-cell">{{ $ev->getPeriod() }}</td>
                                 <td class="p-1 text-center md:table-cell">
                                     @if ($ev->eventType && $ev->eventType->is_all_day)
-                                        <x-jet-checkbox
-                                            wire:click="toggleAuthorization({{ $ev->id }})"
-                                            :checked="$ev->is_authorized"
-                                            :disabled="!$isTeamAdmin"
-                                        />
+                                        <input type="checkbox" wire:click="toggleAuthorization({{ $ev->id }})"
+                                            @checked($ev->is_authorized) @disabled(!$isTeamAdmin) />
                                     @endif
                                 </td>
 
                                 @if (!$isInspector || $isTeamAdmin)
                                     <td class="flex justify-center items-center p-1 md:table-cell">
                                         <div class="flex float-right">
-                                            <a class="btn {{ $ev->is_open ? 'btn-blue' : 'btn-gray' }}" wire:click="edit({{ $ev }})">
+                                            <a class="btn {{ $ev->is_open ? 'btn-blue' : 'btn-gray' }}"
+                                                wire:click="edit({{ $ev }})">
                                                 <i class="fas fa-edit"></i>
                                             </a>
-                                            <a class="btn {{ $ev->is_open ? 'btn-green' : 'btn-gray' }}" wire:click="alertConfirm({{ $ev }})">
+                                            <a class="btn {{ $ev->is_open ? 'btn-green' : 'btn-gray' }}"
+                                                wire:click="alertConfirm({{ $ev }})">
                                                 <i class="fas fa-check"></i>
                                             </a>
-                                            <a class="btn {{ $ev->is_open ? 'btn-red' : 'btn-gray' }}" wire:click="alertDelete({{ $ev }})">
+                                            <a class="btn {{ $ev->is_open ? 'btn-red' : 'btn-gray' }}"
+                                                wire:click="alertDelete({{ $ev }})">
                                                 <i class="fas fa-trash"></i>
                                             </a>
                                         </div>
