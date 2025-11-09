135e0308b1be96d92a5227503036bf6389fa4155
dinamizador <zafarraya@puntosvuela.es>
2025-11-09 22:42:08 +0100

    Add mobile icon back to desktop view, now available on both desktop and mobile

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -89,6 +89,19 @@
                     @livewire('notification-icon')
                 </div>
 
+                <!-- Quick switch to mobile UI -->
+                <div class="ml-3 flex items-center">
+                    <a href="{{ route('mobile.home') }}"
+                       title="{{ __('ui.layout.open_mobile') }}"
+                       class="relative inline-flex items-center p-2 rounded-md hover:bg-gray-50 hover:text-gray-700 text-gray-600 transition-colors duration-200"
+                       data-tooltip="Abrir versión móvil">
+                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M7 2a1 1 0 00-1 1v12a1 1 0 001 1h6a1 1 0 001-1V3a1 1 0 00-1-1H7z"></path>
+                            <path d="M3 6h14v2H3V6z" fill-opacity="0.6"></path>
+                        </svg>
+                    </a>
+                </div>
+
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
                     <x-jet-dropdown align="right" width="48">
