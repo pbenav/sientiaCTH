8706d781a300fdb2fdb749b22d782e465693ddc6
dinamizador <zafarraya@puntosvuela.es>
2025-11-09 22:26:18 +0100

    Make monitor and user menu icons consistent: same size, hover effects, and styling

--- resources/views/mobile/layout.blade.php
+++ resources/views/mobile/layout.blade.php
@@ -85,8 +85,8 @@
                 
                 <!-- User Menu -->
                 <div class="relative">
-                <button onclick="toggleUserMenu()" class="btn-mobile rounded-full">
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20">
+                <button onclick="toggleUserMenu()" class="w-8 h-8 flex items-center justify-center btn-mobile rounded-full hover:bg-blue-700 transition-colors">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path>
                     </svg>
                 </button>
