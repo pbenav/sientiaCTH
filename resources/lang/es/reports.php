a6f85163a356902abe35cd77453362cde312eb96
Pablo Benavides <pbenav@gmail.com>
2025-11-24 13:34:02 +0100

    Crear archivo de traducciones dedicado para informes PDF

---
+++ resources/lang/es/reports.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Report of Events' => 'Informe de Eventos',
+    'Generated on' => 'Generado el',
+    'Total Records' => 'Total de Registros',
+    'Page' => 'Página',
+    'Name' => 'Nombre',
+    'Start' => 'Inicio',
+    'End' => 'Fin',
+    'Duration' => 'Duración',
+    'Description' => 'Descripción',
+    'Observations' => 'Observaciones',
+];
